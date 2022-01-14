955b18bd37ce1296208c40063c557de02b9a61be
Eliel Mariano <eliel.mariano@engenharia.ufjf.br>
2022-01-13 22:55:22 -0300

    continuação da aula de renderização de listas

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -12,13 +12,48 @@ const MainContainer = styled.div`
 class App extends React.Component {
 
   state = {
-    
-  }
+    listaUsuariosInsta4: [
+      {
+        nomeUsuario:'mônica',
+        fotoUsuario:'https://lojasbrilhante.vteximg.com.br/arquivos/ids/165855-1000-1000/totem-turma-da-monica-1.png?v=637225770905070000',
+        fotoPost:'https://www.mundoecologia.com.br/wp-content/uploads/2019/11/Filhote-de-Golden-Retriever-2.jpg'
+      },
+      {
+        nnomeUsuario:'kalinka',
+        fotoUsuario:'https://imagem.natelinha.uol.com.br/tudo-sobre/kalinka_7643.jpeg',
+        fotoPost:'https://i.pinimg.com/originals/dc/4c/7e/dc4c7ef452fe42257f19f0f3a193faad.jpg'
+      },
+      {
+        nomeUsuario:'cleo',
+        fotoUsuario:'http://static.glamurama.uol.com.br/2016/09/cleo-pires.jpg',
+        fotoPost:'https://img.r7.com/images/cleo-07012021132242220'
+      }
+    ]
+  };   
+  
 
 
   render() {
+
+    const listaInsta4 = this.state.listaUsuariosInsta4.map((item, indice)=>{
+      return <Post key={indice}
+      nomeUsuario={item.nomeUsuario}
+      fotoUsuario={item.fotoUsuario}
+      fotoPost={item.fotoPost}
+      />
+                
+    })
+
+    
     return (
+
       <MainContainer>
+        {listaInsta4}
+      </MainContainer>
+
+
+      /* sem renderizar listas
+       <MainContainer>
         <Post
           nomeUsuario={'mônica'}
           fotoUsuario={'https://lojasbrilhante.vteximg.com.br/arquivos/ids/165855-1000-1000/totem-turma-da-monica-1.png?v=637225770905070000'}
@@ -34,9 +69,9 @@ class App extends React.Component {
           fotoUsuario={'http://static.glamurama.uol.com.br/2016/09/cleo-pires.jpg'}
           fotoPost={'https://img.r7.com/images/cleo-07012021132242220'}
         />
-      </MainContainer>
+      </MainContainer> */
     );
-  }
+  };
 }
 
 export default App;
