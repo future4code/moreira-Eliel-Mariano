4d387081d0d63827e94a8ae92a74e3409c5706b2
Eliel Mariano <eliel.mariano@engenharia.ufjf.br>
2022-04-14 21:50:09 -0300

    labecomerce inicio

---
+++ modulo4/labEcommerce-back/src/endpoints/get.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const funcaoGet  = ( async (req:Request, res:Response):Promise< any >=>{
+
+    try {
+        
+        await
+
+    } catch (error) {
+        
+    }
+
+})
