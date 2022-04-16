928e451254420ca44896cacbce03e61861dbbd3f
Eliel Mariano <eliel.mariano@engenharia.ufjf.br>
2022-04-16 15:20:05 -0300

    4 endpoints completos

--- modulo4/labEcommerce-back/src/index.ts
+++ modulo4/labEcommerce-back/src/index.ts
@@ -1,4 +1,15 @@
 import {app} from "./app"
-import { funcaoGet } from "./endpoints/get"
+import { getProducts } from "./endpoints/getPorducts"
+import { getUsers } from "./endpoints/getUsers"
+import { siginProducts } from "./endpoints/siginProducts"
+import { siginUsers } from "./endpoints/siginUsers"
 
-app.get("/", funcaoGet)
+app.post("/users", siginUsers)
+
+app.get("/users", getUsers)
+
+app.post("/products", siginProducts)
+
+app.get("/products", getProducts)
+
+app.post("") //iniciar registo de compra
---
+++ modulo4/labEcommerce-back/src/types.ts
@@ -0,0 +1,13 @@
+export type Users = {
+    id:string,
+    name:string,
+    email:string,
+    password:string
+}
+
+export type Products = {
+    id:string,
+    name:string,
+    price:number,
+    image_url:string
+}
