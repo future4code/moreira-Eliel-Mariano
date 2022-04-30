0b1b1d7b1d845a626989d57420f5e31e625fc006
Eliel Mariano <eliel.mariano@engenharia.ufjf.br>
2022-04-30 14:44:40 -0300

    requisitos mvp

--- modulo5/cokenu/src/index.ts
+++ modulo5/cokenu/src/index.ts
@@ -1,4 +1,15 @@
 import {app} from "./app"
-import { funcaoGet } from "./endpoints/get"
+import { createRecipe } from "./endpoints/endpoints/createRecipe"
+import { getAnotherUserProfile } from "./endpoints/endpoints/getAnotherUserProfile"
+import { getRecipe } from "./endpoints/endpoints/getRecipe"
+import { getUserProfile } from "./endpoints/endpoints/getUserProfile"
+import { login } from "./endpoints/endpoints/login"
+import { signup } from "./endpoints/endpoints/signup"
 
-app.get("/", funcaoGet)
+
+app.post('/signup', signup)
+app.post('/login', login)
+app.get('/user/profile', getUserProfile)
+app.get('/user/:id', getAnotherUserProfile)
+app.post('/recipe', createRecipe)
+app.get('/recipe/:id', getRecipe)
